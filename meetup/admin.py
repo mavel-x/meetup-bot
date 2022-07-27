45b9dd2176ae27e90c9a7734ac19f22030e69dc4
Andrey Sitnikov <84133942+ruandrinho@users.noreply.github.com>
2022-07-27 11:17:08 +0500

    Add Participant and Section admin pages

--- meetup/admin.py
+++ meetup/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Participant, Section, Meeting
 
-# Register your models here.
+
+@admin.register(Participant)
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'company', 'is_speaker')
+    search_fields = ('name', 'company')
+    list_filter = ('is_speaker',)
+
+
+@admin.register(Section)
+class SectionAdmin(admin.ModelAdmin):
+    list_display = ('title',)
