dd42639ec870334b696721b5d5523add1abaa22b
Andrey Sitnikov <84133942+ruandrinho@users.noreply.github.com>
2022-07-28 14:29:57 +0500

    Implement all API functions

--- meetup/views.py
+++ meetup/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
-from meetup.models import Participant
+from django.views.decorators.csrf import csrf_exempt
+from meetup.models import Participant, Section, Meeting, Question
 
 
 def get_participant(request, telegram_id):
@@ -11,28 +12,106 @@ def get_participant(request, telegram_id):
         'company': participant.company,
         'is_speaker': participant.is_speaker
     }
-    return JsonResponse(context, safe=False, json_dumps_params={'ensure_ascii': False, 'indent': 4})
+    return JsonResponse(
+        context,
+        safe=False,
+        json_dumps_params={'ensure_ascii': False, 'indent': 4}
+    )
 
 
+@csrf_exempt
 def register_participant(request):
-    pass
+    participant, created = Participant.objects.get_or_create(
+        name=request.POST.get('name', ''),
+        company=request.POST.get('company', ''),
+        telegram_id=int(request.POST.get('telegram_id', 0))
+    )
+    return JsonResponse(
+        {'status': 'ok' if created else 'error'},
+        safe=False,
+        json_dumps_params={'ensure_ascii': False, 'indent': 4}
+    )
 
 
 def get_sections_list(request):
-    pass
+    sections = []
+    for section in Section.objects.all():
+        sections.append({
+            'id': section.pk,
+            'title': section.title
+        })
+    return JsonResponse(
+        {'sections': sections},
+        safe=False,
+        json_dumps_params={'ensure_ascii': False, 'indent': 4}
+    )
 
 
 def get_section(request, section_id):
-    pass
+    section = get_object_or_404(Section, pk=section_id)
+    meetings = []
+    for meeting in section.meetings.all():
+        meetings.append({
+            'id': meeting.id,
+            'title': meeting.title
+        })
+    context = {
+        'title': section.title,
+        'meetings': meetings
+    }
+    return JsonResponse(
+        context,
+        safe=False,
+        json_dumps_params={'ensure_ascii': False, 'indent': 4}
+    )
 
 
 def get_meeting(request, meeting_id):
-    pass
+    meeting = get_object_or_404(Meeting, pk=meeting_id)
+    speakers = []
+    for speaker in meeting.speakers.all():
+        speakers.append({
+            'telegram_id': speaker.telegram_id,
+            'name': speaker.name,
+            'company': speaker.company
+        })
+    context = {
+        'title': meeting.title,
+        'speakers': speakers
+    }
+    return JsonResponse(
+        context,
+        safe=False,
+        json_dumps_params={'ensure_ascii': False, 'indent': 4}
+    )
 
 
+@csrf_exempt
 def create_question(request):
-    pass
+    participant_telegram_id = request.POST.get('participant_telegram_id', 0)
+    speaker_telegram_id = request.POST.get('speaker_telegram_id', 0)
+    question = request.POST.get('question', '')
+    participant = get_object_or_404(Participant, telegram_id=participant_telegram_id)
+    speaker = get_object_or_404(Participant, telegram_id=speaker_telegram_id)
+    question, created = Question.objects.get_or_create(
+        question=question,
+        participant=participant,
+        speaker=speaker
+    )
+    return JsonResponse(
+        {'status': 'ok' if created else 'error'},
+        safe=False,
+        json_dumps_params={'ensure_ascii': False, 'indent': 4}
+    )
 
 
-def add_answer_to_question(request):
-    pass
+@csrf_exempt
+def add_answer_to_question(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer = request.POST.get('answer', '')
+    question.save()
+    return JsonResponse(
+        {'status': 'ok'},
+        safe=False,
+        json_dumps_params={'ensure_ascii': False, 'indent': 4}
+    )
